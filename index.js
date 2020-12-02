836fa1b95990684015c5dadde8c6be3eb64c108a
zbzyy <75342664+zbzyy@users.noreply.github.com>
2020-12-02 02:48:41 +0000

    Update index.js
    easier to understand

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const { request } = require('gaxios');
+const request = require('request');
 const config = require('./config.json');
 
 const statuses = config.statuses;
 var status_index = 0;
 
 setInterval(() => {
-    const settings = {
+    request({
         method: 'PATCH',
         url: 'https://discordapp.com/api/v6/users/@me/settings',
+        json: true,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36',
             'Authorization': config.token,
             'Content-Type': 'application/json'
         },
-        data: {
+        body: {
             'custom_status': {
                 'text': statuses[status_index++ % statuses.length],
                 'expires_at': null
             }
         }
-    }
-    request(settings);
-    console.log('status updated')
+    })
 }, 15000)
-
-
